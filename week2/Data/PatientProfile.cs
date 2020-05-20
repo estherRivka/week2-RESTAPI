83e72938357e5bb353f7c59bc1bad98abf93cb7b
estherRivka <estherivka1@gmail.com>
2020-05-20 09:56:21 +0300

    upload first version of project

---
+++ week2/Data/PatientProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using week2.Data.Entities;
+using week2.Models;
+
+namespace week2.Data
+{
+    public class PatientProfile : Profile
+    {
+        public PatientProfile()
+        {
+            this.CreateMap<Patient, PatientModel>()
+                .ForMember(dest => dest.PatientId, opt => opt.MapFrom(m => m.Id))
+                //.ForMember(dest=>dest.Paths, )
+                .ReverseMap()
+                .ForMember(dest => dest.Moniker, opt => opt.Ignore());
+       
+            this.CreateMap<Path, PathModel>()
+                .ReverseMap()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+        }
+    }
+}
